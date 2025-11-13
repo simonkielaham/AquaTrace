c616ada51d55e11853056d7beade3dd2ddc2af1b
Simon Kiela <simonkiela@gmail.com>
2025-11-13 04:20:22 +0000

    the left column overlaps the data in the main window

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -318,11 +318,28 @@ const SidebarInset = React.forwardRef<
   HTMLDivElement,
   React.ComponentProps<"main">
 >(({ className, ...props }, ref) => {
+  const { isMobile, state } = useSidebar()
+  
+  if (isMobile) {
+    return (
+       <main
+        ref={ref}
+        className={cn(
+          "relative flex min-h-svh flex-1 flex-col bg-background",
+          className
+        )}
+        {...props}
+      />
+    )
+  }
+
   return (
     <main
       ref={ref}
       className={cn(
-        "relative flex min-h-svh flex-1 flex-col bg-background",
+        "relative flex min-h-svh flex-1 flex-col bg-background transition-[margin-left] duration-200 ease-linear",
+        "peer-data-[state=expanded]:ml-[--sidebar-width]",
+        "peer-data-[state=collapsed]:ml-[--sidebar-width-icon]",
         "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
         className
       )}
