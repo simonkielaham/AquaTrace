b6808846b8b274c1598b63cf7582833be5c7fb2c
Simon Kiela <simonkiela@gmail.com>
2025-11-17 19:20:58 +0000

    Try fixing this error: `Runtime Error: Error: Label is not defined. Erro

--- src/components/dashboard/deployment-list.tsx
+++ src/components/dashboard/deployment-list.tsx
@@ -43,6 +43,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import {
   Dialog,
   DialogContent,
